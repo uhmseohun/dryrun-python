66cce55ebaa8f79da1e0027f856809e1021b96e2
엄서훈 <uhmseohun@gmail.com>
2022-09-03 11:52:22 +0900

    include readme.md to pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 import setuptools
 
+from pathlib import Path
+
+long_description = (Path(__file__).parent / "readme.md").read_text()
+
 setuptools.setup(
     name="dryrun",
-    version="1.0.0",
+    version="1.0.1",
     author="Seohun Uhm",
     author_email="uhmseohun@gmail.com",
     description="Very awesome dryrun module for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/uhmseohun/dryrun-python",
     packages=setuptools.find_packages(),
     classifiers=[
